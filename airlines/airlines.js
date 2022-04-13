babd039ed1fd3e343750a6e094f39d9723cb7c5c
Cristian Bermúdez <ccbabcn@gmail.com>
2022-04-13 20:05:34 +0200

    solved problems on airlin.js

--- airlines/airlines.js
+++ airlines/airlines.js
@@ -1,62 +1,77 @@
-function getUserName(){
-    const INPUTALERT = 'Parece que no has introducido nada, vuelve a intentarlo';
-    let nameRegistered = false;
-    do {
-        const USERNAME = prompt('Please enter your name', 'User');
-        if( USERNAME === null || USERNAME === undefined || USERNAME == ''){
-            alert(INPUTALERT);
-        } else{
-            nameRegistered = true;
-            return USERNAME;
-        }
-    } while (nameRegistered === false);
-}
-
-function displayFlights (){
-    console.log('Aquí tienes información de todos los vuelos disponibles:');
-    flights.forEach(flights => console.log(`
-    El vuelo con origen: ${flights.from}, y destino: ${flights.to}, 
-    tiene un coste de ${flights.cost}€ y ${flights.scale? 'realiza alguna' : 'no realiza ninguna'} escala.`));
-}
-
-function mediumPrice (){
-    let sumOfPrices = 0;
-    for (i = 0; i < flights.length; i++){
-         sumOfPrices += flights[i].cost;
-    }
-    return sumOfPrices/flights.length;
-}
-
-function layOverCounter(){
-    let counter = 0;
-    for (i = 0; i < flights.length; i++){
-        flights[i].scale === true? counter += 1:counter;
-    }
-    return counter;
-}
-
-function lastFiveFlightsDestination(){
-    console.log('Los últimos 5 vuelos tienen como destino:');
-    let lastFlights = flights.slice(-5);
-    lastFlights.forEach( lastFlights => console.log(lastFlights.to));
-}
-
-let flights = [
-    { id: 00, to: 'Bilbao', from: 'Barcelona', cost: 1600, scale: false },
-    { id: 01, to: 'New York', from: 'Barcelona', cost: 700, scale: false },
-    { id: 02, to: 'Los Angeles', from: 'Madrid', cost: 1100, scale: true },
-    { id: 03, to: 'Paris', from: 'Barcelona', cost: 210, scale: false },
-    { id: 04, to: 'Roma', from: 'Barcelona', cost: 150, scale: false },
-    { id: 05, to: 'London', from: 'Madrid', cost: 200, scale: false },
-    { id: 06, to: 'Madrid', from: 'Barcelona', cost: 90, scale: false },
-    { id: 07, to: 'Tokyo', from: 'Madrid', cost: 1500, scale: true },
-    { id: 08, to: 'Shangai', from: 'Barcelona', cost: 800, scale: true },
-    { id: 09, to: 'Sydney', from: 'Barcelona', cost: 150, scale: true },
-    { id: 10, to: 'Tel-Aviv', from: 'Madrid', cost: 150, scale: false }
+const flights = [
+  { id: 0, to: "Bilbao", from: "Barcelona", cost: 1600, scale: false },
+  { id: 1, to: "New York", from: "Barcelona", cost: 700, scale: false },
+  { id: 2, to: "Los Angeles", from: "Madrid", cost: 1100, scale: true },
+  { id: 3, to: "Paris", from: "Barcelona", cost: 210, scale: false },
+  { id: 4, to: "Roma", from: "Barcelona", cost: 150, scale: false },
+  { id: 5, to: "London", from: "Madrid", cost: 200, scale: false },
+  { id: 6, to: "Madrid", from: "Barcelona", cost: 90, scale: false },
+  { id: 7, to: "Tokyo", from: "Madrid", cost: 1500, scale: true },
+  { id: 8, to: "Shangai", from: "Barcelona", cost: 800, scale: true },
+  { id: 9, to: "Sydney", from: "Barcelona", cost: 150, scale: true },
+  { id: 10, to: "Tel-Aviv", from: "Madrid", cost: 150, scale: false },
 ];
 
-console.log(`\n***************************************\n${getUserName()} welcome to Skylab Airlines! ✈️\n***************************************`)
-displayFlights();
-console.log(`\n***************************************\nEl precio medio de los vuelos es ${Number(mediumPrice().toFixed(2))}`);
-console.log(`Hay un total de ${layOverCounter()} vuelos con escala.`)
+function getUserName(userName) {
+  const inputAlert = "Parece que no has introducido nada, vuelve a intentarlo";
+
+  if (userName === null || userName === undefined || userName === "") {
+    return inputAlert;
+  }
+
+  return userName;
+}
+
+function displayFlights() {
+  const flightsDisplay = [];
+  flights.forEach((flight) => {
+    flightsDisplay[flight.id] = `El vuelo con origen: ${
+      flight.from
+    }, y destino: ${flight.to}, 
+    tiene un coste de ${flight.cost}€ y ${
+      flight.scale ? "realiza alguna" : "no realiza ninguna"
+    } escala.`;
+  });
+}
+
+function mediumPrice() {
+  let sumOfPrices = 0;
+  for (let i = 0; i < flights.length; i++) {
+    sumOfPrices += flights[i].cost;
+  }
+  return sumOfPrices / flights.length;
+}
+
+function layOverCounter() {
+  let counter = 0;
+  for (let i = 0; i < flights.length; i++) {
+    if (flights[i].scale === true) {
+      counter += 1;
+    }
+  }
+  return counter;
+}
+
+function lastFiveFlightsDestination() {
+  const lastFlightsText = "Los últimos 5 vuelos tienen como destino: ";
+  const lastFlightsDestinations = [];
+  const lastFlights = flights.slice(-5);
+  lastFlights.forEach((lastFlight) =>
+    lastFlightsDestinations.push(lastFlight.to)
+  );
+  return lastFlightsText + lastFlightsDestinations.join("");
+}
+
+function initialGreeting() {
+  const initialMessage = `\n***************************************\n${getUserName()} welcome to Skylab Airlines! ✈️\n***************************************`;
+  displayFlights();
+  const mediumPriceMessage = `\n***************************************\nEl precio medio de los vuelos es ${Number(
+    mediumPrice().toFixed(2)
+  )}`;
+
+  const totalLayOver = `Hay un total de ${layOverCounter()} vuelos con escala.`;
+  return initialMessage + mediumPriceMessage + totalLayOver;
+}
+
+initialGreeting();
 lastFiveFlightsDestination();
