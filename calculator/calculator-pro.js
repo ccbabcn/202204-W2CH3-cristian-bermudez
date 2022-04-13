9d037fb597767730586a9df9e6ea9f5acb52ac0e
Cristian Bermúdez <ccbabcn@gmail.com>
2022-04-13 21:52:46 +0200

    added first function test form calculator-pro.js on testint area

--- calculator/calculator-pro.js
+++ calculator/calculator-pro.js
@@ -1,76 +1,87 @@
-function inputAndControl() {
-    const NOIMPUTLAERT = 'Error! Seems like you did not enter any number.';
-    const NONUMBERALERT = 'Error! Seems like what you enter was not a number.';
-    const KEEPUSINGMESSAGE = 'Do you want to keep using this calculator?';
-    const NEWNUMBERMESSAGE = 'Number registered correctly. Do you want to add another number?';
-    let inputNumbersArray = [];
-    let userStays = false;
-    do {
-        let newInput = prompt('Enter a number');
-        let newNumber = Number(newInput);
-        if( newInput == undefined || newInput == '' || newInput == null){ 
-            alert(NOIMPUTLAERT);
-            userStays = window.confirm(KEEPUSINGMESSAGE);    
-        } else if( Number.isNaN(newNumber) ){ 
-            alert(NONUMBERALERT);
-            userStays = window.confirm(KEEPUSINGMESSAGE);
-        } else {
-            inputNumbersArray.push(newNumber);
-            userStays = window.confirm(NEWNUMBERMESSAGE);
-        }
-    } while (userStays == true)
-    return inputNumbersArray;
+const newNumberInput = "11";
+function inputAndControl(newInput) {
+  const noInputAlert = "Error! Seems like you did not enter any number.";
+  const noNumberAlert = "Error! Seems like what you enter was not a number.";
+  const inputNumbersArray = [];
+
+  const newNumber = Number(newInput);
+  if (newInput === undefined || newInput === "" || newInput === null) {
+    return noInputAlert;
+  }
+  if (Number.isNaN(newNumber)) {
+    return noNumberAlert;
+  }
+  inputNumbersArray.push(newNumber);
+
+  return inputNumbersArray;
 }
 
-function roundToFixed3 (x){
-    let roundedNum;
-    Number.isInteger(x)? roundedNum = x : roundedNum = Number((x).toFixed(3));
-    return roundedNum;
+describe("Given inputAndControl function", () => {
+  describe("When it recieves an undefined value", () => {
+    test("Then it shall return 'Error! Seems like you did not enter any number.'", () => {
+      const undefinedValue = undefined;
+      const expectedResult = "Error! Seems like you did not enter any number.";
+
+      const result = inputAndControl(undefinedValue);
+
+      expect(result).toBe(expectedResult);
+    });
+  });
+});
+
+function roundToFixed3(x) {
+  let roundedNum;
+  Number.isInteger(x) ? (roundedNum = x) : (roundedNum = Number(x.toFixed(3)));
+  return roundedNum;
 }
 
-function calculatorPro (myArr){
-    let operationsArray = [];
-    let resultsArray = [];
-    let calculatorArray = [[],[]];
+function calculatorPro(myArr) {
+  const operationsArray = [];
+  const resultsArray = [];
+  let calculatorArray = [[], []];
+  let messageToUser = "";
 
-    if (myArr.length === 1){
-        console.log(`******************************************\nYou only entered the number ${myArr[0]}`);
-      
-        operationsArray.push("It's square root is: ");
-        resultsArray.push(roundToFixed3(Math.sqrt(myArr[0])));
-    } else if (myArr.length > 1){
-        console.log(`******************************************\nYou entered a total of ${myArr.length} numbers (${myArr})`);
+  if (myArr.length === 1) {
+    messageToUser = `******************************************\nYou only entered the number ${myArr[0]}`;
 
-        operationsArray.push("Their sum is: ");
-        resultsArray.push(roundToFixed3(myArr.reduce(function(a, b){ return a + b; })));
+    operationsArray.push("It's square root is: ");
+    resultsArray.push(roundToFixed3(Math.sqrt(myArr[0])));
+  } else if (myArr.length > 1) {
+    messageToUser = `******************************************\nYou entered a total of ${myArr.length} numbers (${myArr})`;
 
-        operationsArray.push("Their substraction is: ");
-        resultsArray.push(roundToFixed3(myArr.reduce(function(a, b){ return a - b; })));
+    operationsArray.push("Their sum is: ");
+    resultsArray.push(roundToFixed3(myArr.reduce((a, b) => a + b)));
 
-        operationsArray.push("Their multiplication is: ");
-        resultsArray.push(roundToFixed3(myArr.reduce(function(a, b){ return a * b; })));
+    operationsArray.push("Their substraction is: ");
+    resultsArray.push(roundToFixed3(myArr.reduce((a, b) => a - b)));
 
-        operationsArray.push("Their divition is: ");
-        resultsArray.push(roundToFixed3(myArr.reduce(function(a, b){ return a / b; })));
-    }
-    return calculatorArray = [operationsArray, resultsArray];
+    operationsArray.push("Their multiplication is: ");
+    resultsArray.push(roundToFixed3(myArr.reduce((a, b) => a * b)));
+
+    operationsArray.push("Their divition is: ");
+    resultsArray.push(roundToFixed3(myArr.reduce((a, b) => a / b)));
+  }
+  calculatorArray = [operationsArray, resultsArray];
+  return messageToUser + calculatorArray;
 }
 
-function showOnConsole (myArr){
-    for (let i = 0; i < myArr[0].length; i++){
-        console.log(`${myArr[0][i]}${myArr[1][i]}`);
-    }
+function showOnConsole(myArr) {
+  const stringDisplay = [];
+  for (let i = 0; i < myArr[0].length; i++) {
+    stringDisplay.push(`${myArr[0][i]}${myArr[1][i]}`);
+  }
+  return stringDisplay;
 }
 
-function calculatorLooper (){
-    const ENDMESSAGE = 'Calculator finished.';
-    const KEEPUSINGMESSAGE = 'Do you want to start a new set of numbers?';
-    let finish = false;
-    do{
-        showOnConsole(calculatorPro(inputAndControl()));
-        alert(ENDMESSAGE);
-        finish = window.confirm(KEEPUSINGMESSAGE);  
-    } while (finish == true)
+const finish = false;
+function calculatorLooper(isFinish) {
+  const endMessage = "Calculator finished.";
+  const keepUsingMessage = "Do you want to start a new set of numbers?";
+  if (isFinish === false) {
+    showOnConsole(calculatorPro(inputAndControl(newNumberInput)));
+    return keepUsingMessage;
+  }
+  return endMessage;
 }
 
-calculatorLooper ();
+calculatorLooper();
